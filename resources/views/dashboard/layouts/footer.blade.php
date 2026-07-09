e34fa04f70c6af4ae07e542839efdcb7bd44682e
Arief Andreawan <ariefandreawan024@gmail.com>
2026-07-09 16:25:20 +0700

    Sembunyikan footer copyright

--- resources/views/dashboard/layouts/footer.blade.php
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -7,9 +7,9 @@
                     <script>
                         document.write(new Date().getFullYear() + ",");
                     </script>
-                    made with <i class="ri-heart-fill text-red-500"></i> by
-                    <a href="https://github.com/Akbarwp" class="font-semibold text-gray-500 dark:text-purple-200" target="_blank">AkbarWP</a>
-                    for a better web.
+                    <!-- made with <i class="ri-heart-fill text-red-500"></i> by -->
+                    <!-- <a href="https://github.com/Akbarwp" class="font-semibold text-gray-500 dark:text-purple-200" target="_blank">AkbarWP</a>
+                    for a better web. -->
                 </div>
             </div>
         </div>
